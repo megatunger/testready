1ba7c2f5b0485a12159a00f5a2dae4b4991baa91
lymin18 <lymin1808@gmail.com>
2019-11-23 18:29:47 +0700

    student course

--- ExamReg/app/helpers/dashboard_student_helper.rb
+++ ExamReg/app/helpers/dashboard_student_helper.rb
@@ -1,2 +1,13 @@
 module DashboardStudentHelper
+  def checkBanned(course)
+    return CourseStudent.where(student_id: @student.id, course_id: course.id).first.banned
+  end
+
+  def returnColor(course)
+    return checkBanned(course)? "danger" : "success"
+  end
+
+  def returnText(course)
+    return checkBanned(course)? "Bạn đã bị cấm thi!" : "Đăng ký"
+  end
 end
