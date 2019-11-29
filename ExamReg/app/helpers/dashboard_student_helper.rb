8943aec1cb4a1e24a78041db17066432a284b10e
lymin18 <lymin1808@gmail.com>
2019-11-29 16:35:32 +0700

    abc

--- ExamReg/app/helpers/dashboard_student_helper.rb
+++ ExamReg/app/helpers/dashboard_student_helper.rb
@@ -3,11 +3,37 @@ module DashboardStudentHelper
     return CourseStudent.where(student_id: @student.id, course_id: course.id).first.banned
   end
 
+  def checkRegistration(course)
+    Registration.all.each_with_index do |registration|
+      if registration.student_id == @student.id
+          course.exam_schedules.each_with_index do |exam_schedule|
+            if registration.exam_schedule_id = exam_schedule.id
+              puts exam_schedule.id
+              return true
+            end
+          end
+      end
+    end
+    return false
+  end
+
   def returnColor(course)
-    return checkBanned(course)? "danger" : "success"
+    # return checkBanned(course)? "danger" : "warning"
+    if checkBanned(course)
+      return "danger"
+    elsif checkRegistration(course)
+      return "success"
+    else return "warning"
+    end
   end
 
   def returnText(course)
-    return checkBanned(course)? "Bạn đã bị cấm thi!" : "Đăng ký"
+    if checkBanned(course)
+      return "Bạn đã bị cấm thi!"
+    elsif checkRegistration(course)
+      return "Bạn đã đăng ký thành công!"
+    else return "Đăng ký"
+    end
+    # return checkBanned(course)? "Bạn đã bị cấm thi!" : "Đăng ký"
   end
 end
