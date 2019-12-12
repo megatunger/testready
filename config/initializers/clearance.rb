e506f408ad654b11d47829385c5475d73783406c
Tung <megatunger@gmail.com>
2019-12-12 21:03:37 +0700

    Update routes

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,5 @@
 Clearance.configure do |config|
-  config.routes = false
+  config.routes = true
   config.mailer_sender = "noreply@megatunger.codes"
   config.rotate_csrf_on_sign_in = true
   config.allow_sign_up = false
