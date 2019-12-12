41d5f241b3a7df2bbafa1ea0b83b1f4152047b7e
Tung <megatunger@gmail.com>
2019-12-12 20:13:40 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://megatunger.codes" }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
